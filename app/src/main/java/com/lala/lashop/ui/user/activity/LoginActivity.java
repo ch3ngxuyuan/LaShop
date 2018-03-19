f3beeb2c6b62dfaa28d258bf6c37aaca05151579
JX <913835745@qq.com>
2018-03-19 17:44:40 +0800

    注册登录

---
+++ app/src/main/java/com/lala/lashop/ui/user/activity/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.lala.lashop.ui.user.activity;
+
+import com.lala.lashop.R;
+import com.lala.lashop.base.BaseActivity;
+
+/**
+ * Created by JX on 2018/3/19.
+ */
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    public int setContentView() {
+        return R.layout.login_activity;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+}
---
+++ app/src/main/java/com/lala/lashop/ui/user/activity/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.lala.lashop.ui.user.activity;
+
+import com.lala.lashop.R;
+import com.lala.lashop.base.BaseActivity;
+
+/**
+ * 注册
+ * Created by JX on 2018/3/19.
+ */
+
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    public int setContentView() {
+        return R.layout.register_activity;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+}
