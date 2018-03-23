84ef8b59c4f3d7a73c7fee1fe64b1df296a9595a
CJX <913835745@qq.com>
2018-03-23 21:34:42 +0800

    订单

---
+++ app/src/main/java/com/lala/lashop/ui/user/fragment/IndentFragment.java
@@ -0,0 +1,21 @@
+package com.lala.lashop.ui.user.fragment;
+
+import com.lala.lashop.R;
+import com.lala.lashop.base.BaseFragment;
+
+/**
+ * Created by JX on 2018/3/23.
+ */
+
+public class IndentFragment extends BaseFragment {
+
+    @Override
+    public int setContentView() {
+        return R.layout.indent_fragment;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+}
